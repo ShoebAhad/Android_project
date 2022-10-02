449c54f0cefa644529254354b883a74876ad0826
MJI-Abir <mjiabir12007@gmail.com>
2022-10-03 02:20:39 +0600

    added bottom navigation menu to main.xml

--- app/src/main/java/com/app/hotel/MainActivity.java
+++ app/src/main/java/com/app/hotel/MainActivity.java
@@ -15,38 +15,38 @@ import android.widget.TextView;
 import java.util.Calendar;
 
 public class MainActivity extends splash_activity {
-    TextView textView;
-    DatePickerDialog.OnDateSetListener setListener;
+//    TextView textView;
+//    DatePickerDialog.OnDateSetListener setListener;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.datepicker);
-        Calendar calender = Calendar.getInstance();
-        final int year = calender.get(Calendar.YEAR);
-        final int month = calender.get(Calendar.MONTH);
-        final int day = calender.get(Calendar.DAY_OF_MONTH);
-
-
-        textView.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth, setListener, year, month, day);
-                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                datePickerDialog.show();
-            }
-        });
-        setListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayofmonth) {
-                    month = month + 1;
-                 String date = day + "/" + month + "/" + year;
-                 textView.setText(date);
-
-            }
-        };
-/*        public void openActivity2() {
-            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-        } */
+//        textView = findViewById(R.id.datepicker);
+//        Calendar calender = Calendar.getInstance();
+//        final int year = calender.get(Calendar.YEAR);
+//        final int month = calender.get(Calendar.MONTH);
+//        final int day = calender.get(Calendar.DAY_OF_MONTH);
+//
+//
+//        textView.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View view) {
+//                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth, setListener, year, month, day);
+//                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+//                datePickerDialog.show();
+//            }
+//        });
+//        setListener = new DatePickerDialog.OnDateSetListener() {
+//            @Override
+//            public void onDateSet(DatePicker view, int year, int month, int dayofmonth) {
+//                    month = month + 1;
+//                 String date = day + "/" + month + "/" + year;
+//                 textView.setText(date);
+//
+//            }
+//        };
+///*        public void openActivity2() {
+//            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+//        } */
 
     }
 }
