6516d3c9203b63bff99ec987dc861812a783a0aa
MJI-Abir <mjiabir12007@gmail.com>
2022-10-08 03:28:41 +0600

    register design changed

--- app/src/main/java/com/app/hotel/activities/guest_size.java
+++ app/src/main/java/com/app/hotel/activities/guest_size.java
@@ -1,14 +1,17 @@
-package com.app.hotel;
+package com.app.hotel.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.app.hotel.R;
 
 public class guest_size extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guest_size);
     }
+
 }
