d9400986d6ce233f93e144f359818425b5958a49
MJI-Abir <mjiabir12007@gmail.com>
2022-10-16 02:55:38 +0600

    fixed

--- app/src/main/java/com/app/hotel/activities/GuestActivity.java
+++ app/src/main/java/com/app/hotel/activities/GuestActivity.java
@@ -3,15 +3,10 @@ package com.app.hotel.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-<<<<<<< HEAD
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-=======
-import android.os.Bundle;
-import android.view.View;
->>>>>>> refs/remotes/origin/master
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -20,10 +15,7 @@ import com.app.hotel.R;
 public class GuestActivity extends AppCompatActivity  implements View.OnClickListener {
     TextView value;
     ImageButton increment, decrement;
-<<<<<<< HEAD
     Button done;
-=======
->>>>>>> refs/remotes/origin/master
     int count = 0;
 
     @SuppressLint("WrongViewCast")
@@ -34,17 +26,12 @@ public class GuestActivity extends AppCompatActivity  implements View.OnClickLis
         value = (TextView) findViewById(R.id.adult);
         increment = findViewById(R.id.plus);
         decrement = findViewById(R.id.minus);
-<<<<<<< HEAD
 
         done = findViewById(R.id.done);
         done.setOnClickListener(view ->
                 startActivity(new Intent(GuestActivity.this, MainActivity.class)));
 
         increment.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("SetTextI18n")
-=======
-        increment.setOnClickListener(new View.OnClickListener() {
->>>>>>> refs/remotes/origin/master
             @Override
             public void onClick(View view) {
                 count++;
@@ -53,13 +40,8 @@ public class GuestActivity extends AppCompatActivity  implements View.OnClickLis
 
 
         });
-<<<<<<< HEAD
 
         decrement.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("SetTextI18n")
-=======
-        decrement.setOnClickListener(new View.OnClickListener() {
->>>>>>> refs/remotes/origin/master
             @Override
             public void onClick(View view) {
                 if (count <= 0) {
